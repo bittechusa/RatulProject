793ada0a517687d0dc6d5db2dd89692fa0f7caad
ratul <ratul@gmail.com>
2016-06-21 12:01:32 +0600

    added

---
+++ BASE4/src/main/java/BASE4/BASE4/App.java
@@ -0,0 +1,13 @@
+package BASE4.BASE4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
